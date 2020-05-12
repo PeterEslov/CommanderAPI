b85d407785ffb0b692a22970df67717f46e74ee3
Peter Larsson <peter.eslov@hotmail.se>
2020-05-12 08:42:08 +0200

    Added Command Model to API Project

--- src/CommandAPI/Models/Command.cs
+++ src/CommandAPI/Models/Command.cs
@@ -6,6 +6,6 @@ namespace CommandAPI.Models
         public string HowTo { get; set; }
         public string Platform { get; set; }
         public string CommandLine { get; set; }
-        
+
     }
 }
